02f5fd7117ea45eb3087fac48dca4a912dd8d794
Dima <dima23790@gmail.com>
2020-04-29 21:25:32 +0300

    used lodash memoize function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { FETCH_POSTS, FETCH_USER } from '../types/index';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
@@ -6,8 +7,12 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: FETCH_POSTS, payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: FETCH_USER, payload: response.data });
-};
+});
