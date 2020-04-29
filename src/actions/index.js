1399b01d5afa89f945875d36e8b2b4112771b9fd
Dima <dima23790@gmail.com>
2020-04-29 21:10:03 +0300

    implemented fetching specific user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,13 @@
-import { FETCH_POSTS } from '../types/index';
+import { FETCH_POSTS, FETCH_USER } from '../types/index';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get('/posts');
-  dispatch({ type: FETCH_POSTS, payload: response });
+  dispatch({ type: FETCH_POSTS, payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: FETCH_USER, payload: response.data });
 };
---
+++ src/components/UserHeader.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class UserHeader extends Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
+  render() {
+    const { user } = this.props;
+    if (!user) {
+      return <div>Loading</div>;
+    }
+    return <div className='header'>{user.name}</div>;
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  return { user: state.users.find((user) => user.id === props.userId) };
+};
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
