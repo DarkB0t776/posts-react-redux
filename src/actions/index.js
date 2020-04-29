df4e204954e450f3ac6ecbfa98da55c21da33311
Dima <dima23790@gmail.com>
2020-04-29 20:16:15 +0300

    created posts reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
-import { FETCH_POST } from '../types/index';
+import { FETCH_POSTS } from '../types/index';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get('/posts');
-  dispatch({ type: FETCH_POST, payload: response });
+  dispatch({ type: FETCH_POSTS, payload: response });
 };
